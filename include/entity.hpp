ab5a347c07a6515360da22f8cf226565459e61aa
anapoli2 <anapoli2@ncsu.edu>
2026-09-06 22:05:36 -0400

    Reset main to barebones defaults for submission and added default drawing to draw function

--- include/entity.hpp
+++ include/entity.hpp
@@ -9,14 +9,28 @@
 struct Entity {
     GlobalPosition position;
     Sprite* sprite = nullptr;
+    bool spriteLoaded = false;
+    SDL_FRect defaultRectangle;
+    int color[4] = { 0, 0, 0, 255 };
+    float rectangleWidth = 0.0f;
+    float rectangleHeight = 0.0f;
 
     Collision Collision;
 
     bool hasCollision = false;
 
-    void updatePosition() {
-        // Position is updated by Physics::step()
+    void setColor( int r, int g, int b, int a ) {
+        color[0] = r;
+        color[1] = g;
+        color[2] = b;
+        color[3] = a;
     }
+
+    void setRectangleHeightAndWidth( float h, float w ) {
+        rectangleHeight = h;
+        rectangleWidth = w;
+    }
+
 
     void setCollisionBox(float halfWidth, float halfHeight) {
         Vector2 center = position.position;
@@ -24,11 +38,18 @@ struct Entity {
         Collision.TopRight = { center.x + halfWidth, center.y + halfHeight };
         Collision.TopLeft = { center.x - halfWidth, center.y + halfHeight };
         Collision.BottemRight = { center.x + halfWidth, center.y - halfHeight };
+        rectangleWidth = halfWidth * 2.0f;
+        rectangleHeight = halfHeight * 2.0f;
     }
 
     void draw(SDL_Renderer* renderer) {
-        if (sprite != nullptr) {
+        if (sprite != nullptr && spriteLoaded ) {
             sprite->draw(renderer, position.position.x, position.position.y);
+        } else {
+            defaultRectangle = { position.position.x - rectangleWidth / 2.0f, position.position.y - rectangleHeight / 2.0f, 
+                                 rectangleWidth, rectangleHeight};
+            SDL_SetRenderDrawColor( renderer, color[0], color[1], color[2], color[3] );
+            SDL_RenderFillRect( renderer, &defaultRectangle );
         }
     }
 };
---
+++ src/main.cpp
@@ -0,0 +1,58 @@
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <SDL3_image/SDL_image.h>
+#include <cstdio>
+#include <cmath>
+#include "input.hpp"
+#include "entity.hpp"
+#include "coolPhysics.hpp"
+
+const int WINDOW_WIDTH = 1920;
+const int WINDOW_HEIGHT = 1080;
+const int FRAME_COUNT = 8;
+const int FRAME_WIDTH = 512;
+const int FRAME_HEIGHT = 512;
+const int ANIMATION_DELAY = 100;
+
+
+int main ( int argc, char *argv[] ) {
+
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window * window = nullptr;
+    SDL_Renderer * renderer = nullptr;
+    if (!SDL_CreateWindowAndRenderer("Cool engine", WINDOW_WIDTH, WINDOW_HEIGHT, 0, &window, &renderer)) {
+        SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+    SDL_Log("Window created!");
+
+
+    bool running = true;
+    SDL_Event event;
+
+
+
+    while (running) {
+
+        
+
+
+        SDL_SetRenderDrawColor( renderer, 0, 0, 255, 255 );
+        SDL_RenderClear( renderer );
+
+       
+
+        SDL_RenderPresent( renderer );
+    }
+
+
+    SDL_DestroyRenderer( renderer );
+    SDL_DestroyWindow( window );
+    SDL_Quit();
+    return 0;
+}
