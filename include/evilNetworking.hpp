49802eb1194ecad9493c674859a39984a57f6882
anapoli2 <anapoli2@ncsu.edu>
2026-09-15 21:14:11 -0400

    networking skeelton

--- include/evilNetworking.hpp
+++ include/evilNetworking.hpp
@@ -2,3 +2,32 @@
 #define EVIL_NETWORKING
 
 #include "coolMath.hpp"
+#include <zmq.hpp>
+#include <iostream>
+#include <thread>
+#include <string>
+#include <chrono>
+
+class p2pNetworking {
+    private:
+
+    public:
+};
+
+
+class server {
+    private:
+
+    public:
+}
+
+class client {
+    private:
+
+    public:
+};
+
+
+
+
+#endif
