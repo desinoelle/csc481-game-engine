4db3ec5f7d0a8ea90ca0371188005d588995c357
bacrawfo <bacrawfo@ncsu.edu>
2026-09-25 14:06:52 -0400

    Updated some expressions to use float

--- include/timeMagic.cpp
+++ include/timeMagic.cpp
@@ -9,11 +9,11 @@ void Timeline::init() {
 
 float Timeline::update() {
     Uint64 now = SDL_GetTicks();
-    double delta = (now - lastTicks) / 1000.0;
+    float delta = (now - lastTicks) / 1000.0f;
     lastTicks = now;
 
     if (isPaused()) {
-        return 0.0;
+        return 0.0f;
     } else {
         accumulated += delta * timeScale;
         return accumulated;
